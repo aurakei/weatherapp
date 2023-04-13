9d2395ff99941d02cf498b66528fe6a4a230025c
aurakei <aurakeith@gmail.com>
2023-04-13 11:21:26 +0300

    fetching and mapping data from an API

--- src/api.jsx
+++ src/api.jsx
@@ -6,6 +6,8 @@ export const geoApiOptions = {
     }
   };
 
-  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "f95b3575f5194b482c75d675408a64eb"
 
