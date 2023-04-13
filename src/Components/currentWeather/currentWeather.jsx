bfe4b1ca9f967c415643242d64dea2ca4b6e1710
aurakei <aurakeith@gmail.com>
2023-04-13 10:49:21 +0300

    working on current weather component

--- src/Components/currentWeather/currentWeather.jsx
+++ src/Components/currentWeather/currentWeather.jsx
@@ -14,6 +14,31 @@ export default function CurrentWeather() {
                     <img src="icons/storm.png" alt="weather" className="weatherIcon" />
 
                 </div>
+
+                <div className="bottom">
+                    <p className="temperature">12°C</p>
+                    <div className="details">
+                        <div className="parameterRow">
+                            <span className="parameterLabel top">Details</span>
+                        </div>
+                        <div className="parameterRow">
+                            <span className="parameterLabel">It fels like</span>
+                            <span className="parameterValue">-3°C</span>
+                        </div>
+                        <div className="parameterRow">
+                            <span className="parameterLabel">Wind</span>
+                            <span className="parameterValue">1m/s</span>
+                        </div>
+                        <div className="parameterRow">
+                            <span className="parameterLabel">Humidity</span>
+                            <span className="parameterValue">30%</span>
+                        </div>
+                        <div className="parameterRow">
+                            <span className="parameterLabel">Pressure</span>
+                            <span className="parameterValue">12hPc</span>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
